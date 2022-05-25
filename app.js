2a0c1249d9c1494d8f96d406f20b980a0339b2c7
yaahsin <brillanteoboe@gmail.com>
2022-05-25 21:56:49 +0800

    feat: create new shorten and 404 function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-// const res = require('express/lib/response')
+const bodyParser = require('body-parser')
 const URLlist = require('./models/URL')
 const mongoose = require('mongoose') // 載入 mongoose
 
@@ -22,15 +22,17 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // route setting 首頁
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 // routes setting 取得input value
-app.get('/shorten', (req, res) => {
+app.post('/shorten', (req, res) => {
   // 收到長網址 input req.query
-  const original = req.query.original
+  const original = req.body.original
   // 1. 比對是否有效
   function isValidHttpUrl (string) {
     let url
@@ -43,6 +45,20 @@ app.get('/shorten', (req, res) => {
     }
     // boolean: 回報其協議是htt: 或https:
     return url.protocol === "http:" || url.protocol === "https:";
+  }
+  // 10進位轉62進位 (26大寫+26小寫+10數字)
+  function ShortLink () {
+    let str = '';
+    const arr = [
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+    for (let i = 0; i < 6; i++) {
+      const pos = Math.round(Math.random() * (arr.length - 1));
+      str += arr[pos];
+    }
+    return str;
   }
 
   if (isValidHttpUrl(original)) {
@@ -58,49 +74,37 @@ app.get('/shorten', (req, res) => {
         // 3. 若有, 取出對應短網址
         if (inputURL) {
           shorten = base + inputURL.shorten
-          console.log(shorten)
           return res.render('index', { original, shorten })
+          // 4. 若無, 建立短網址進database
+        } else {
+          shorten = ShortLink()
+          URLlist.create({
+            original: original,
+            shorten: shorten
+          })
         }
       })
   } else {
     console.log("invalid URL")
   }
-
-
-  // 4. 若無, 建立短網址進database
-
-})
-
-function Errorlog (e) {
-  console.log(e)
-}
-
-// 比對新的內容
-app.post('/', (req, res) => {
-  const original = req.query.original
-  const base = "http://localhost:3000/"
-
-  URLlist.find()
-    .lean()
-    .then((URL) => {
-      let inputURL = URL.find((url) => url.original === original)
-    })
-
 })
 
 // route setting shorten to original URL 短網址跳轉到長網址
-app.get('/:shorten', (req, res) => {
-  const shortLink = req.params.shorten
-  URLlist.findOne({ shorten: shortLink })
+app.get("/:shorten", (req, res) => {
+  const { shorten } = req.params;
+  URLlist.findOne({ shorten: shorten })
     .lean()
-    .then((relink => {
+    .then((relink) => {
       if (relink) {
-        console.log(relink)
-        res.status(301).redirect(relink.original)
+        console.log(relink);
+        res.status(301).redirect(relink.original);
+      } else {
+        res.sendStatus(404);
       }
-    }))
-    .catch(() => { res.sendStatus(404) })
-})
+    })
+    .catch(() => {
+    });
+});
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
