9cfb64ee1290a4de56e0718637d4ab9dd1b2fb89
yaahsin <brillanteoboe@gmail.com>
2022-05-25 22:38:35 +0800

    fix shorten logic

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -29,7 +29,7 @@ router.post('/shorten', (req, res) => {
       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
       'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     ];
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 5; i++) {
       const pos = Math.round(Math.random() * (arr.length - 1));
       str += arr[pos];
     }
